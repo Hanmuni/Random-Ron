676081328f46d1a82a1315f1afac7eda5bc28871
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-12-06 15:16:38 +0100

    feat: no duplicate quotes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const getQuote = document.querySelector("#get-quote");
 let quote = document.querySelector("#quote");
+let quotes = [];
 
 let fetchQuote = () => {
   fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
@@ -11,8 +12,16 @@ let fetchQuote = () => {
       }
     })
     .then((data) => {
-      console.log(data);
-      quote.textContent = data[0];
+      console.log("This is the API data", data);
+
+      if (!quotes.includes(data[0])) {
+        quotes.push(data[0]);
+        quote.textContent = data[0];
+      } else {
+        fetchQuote();
+      }
+
+      console.log("This is the quotes array", quotes);
     })
     .catch((error) => {
       console.warn(error);
